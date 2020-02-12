234f367d90d9577563bb418ee7ba227c8611eba9
Brijesh Kumar <brije111@gmail.com>
2020-02-12 13:30:40 +0530

    added default route

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 
-var UserController = require('./user/UserController');
+var UserController = require('./controller/UserController');
+var DefaultController = require('./controller/DefaultController');
 app.use('/users', UserController);
-app.use('/', DefaultController);
+//app.use('/', DefaultController);
 
 module.exports = app;
